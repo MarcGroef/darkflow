9a633681b8c8e39fdb16a1fe3a27e465e9eb068e
Marc Groefsema <marcgroefsema@gmail.com>
2017-03-02 13:01:59 +0100

    added annotations

---
+++ annotate/annotator.py
@@ -0,0 +1,3 @@
+#python bagtoset.py <datasetdir> --topic <topicname>. Should put all images from topic in a JPEGImages folder
+
+import cv
---
+++ annotate/bagtoset.py
@@ -0,0 +1,2 @@
+##python bagtoset.py <datasetdir> --topic <topicname>. Should put all images from topic in a JPEGImages folder
+
